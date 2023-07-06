3dd70194c2ab3640e7c2de41e206f616dbae2ad6
Mauricio Inhaia <mauricio.inhaia@hotmail.com>
2023-07-05 21:55:01 -0300

    Created Project

---
+++ src/main/java/com/need4speed/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.need4speed.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
