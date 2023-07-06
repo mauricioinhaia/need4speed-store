84a0a2be424ca8ca2b813bafbba55228334dae53
Mauricio Inhaia <mauricio.inhaia@hotmail.com>
2023-07-05 22:48:43 -0300

    Created Produto Model

---
+++ src/main/java/com/need4speed/api/models/Produto.java
@@ -0,0 +1,66 @@
+package com.need4speed.api.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, length = 40)
+    private String descricao;
+    @Column(nullable = false)
+    private BigDecimal valor;
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private Boolean servico;
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private Boolean inativo;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Boolean getServico() {
+        return servico;
+    }
+
+    public void setServico(Boolean servico) {
+        this.servico = servico;
+    }
+
+    public Boolean getInativo() {
+        return inativo;
+    }
+
+    public void setInativo(Boolean inativo) {
+        this.inativo = inativo;
+    }
+}
